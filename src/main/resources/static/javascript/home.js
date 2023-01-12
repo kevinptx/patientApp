718bf9809938f8c524d95de74a4b4f65712c3128
Kevin Peery <kevin.peery@gmail.com>
2023-01-11 20:33:52 -0600

    clean up JS

--- src/main/resources/static/javascript/home.js
+++ src/main/resources/static/javascript/home.js
@@ -371,20 +371,6 @@ patientPrescriptionsDisplay.append(drugFields);
 })
 }
 
-const accordionItems = document.querySelectorAll('.accordion-collapse')
-const acc = document.getElementById('accordionExample')
-
-accordionItems.forEach((el)=>{
-    el.addEventListener('shown.bs.collapse',(e)=>{
-        var scrollOffset = acc.scrollTop + el.parentNode.offsetTop
-        acc.scroll({
-            top: scrollOffset,
-            left: 0,
-            behavior: 'smooth'
-        })
-    })
-})
-
 
 
 
