e4acf9dbcbb1b7f7a56965742da1f37e5566c24c
Kevin Peery <kevin.peery@gmail.com>
2023-01-31 22:45:10 -0600

    update front end styling

--- src/main/java/com/hackbright/patientApp/PatientAppApplication.java
+++ src/main/java/com/hackbright/patientApp/PatientAppApplication.java
@@ -2,13 +2,6 @@ package com.hackbright.patientApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-//@EnableJpaRepositories("com.hackbright.patientApp.*")
-//@ComponentScan(basePackages = { "com.hackbright.patientApp.*" })
-//@EntityScan("com.hackbright.patientApp.entities.Doctor")
 @SpringBootApplication
 public class PatientAppApplication {
 
